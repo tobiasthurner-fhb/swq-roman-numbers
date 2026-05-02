7d916b9aa7eb31d434736684b4ca07c3d67a9288
Tobias Thurner <2410859009@hochschule-burgenland.at>
2026-05-02 10:29:15 +0200

    feat: implement RomanNumberConverter

--- src/main/java/at/hochschule/burgenland/RomanNumberConverter.java
+++ src/main/java/at/hochschule/burgenland/RomanNumberConverter.java
@@ -1,7 +1,22 @@
 package at.hochschule.burgenland;
 
 public class RomanNumberConverter {
-    public static String toRomanNumbers(int number){
-        return "";
+    public static String toRomanNumbers(int number) {
+        if (number < 1 || number > 3000) {
+            throw new IllegalArgumentException("Number must be between 1 and 3000");
+        }
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanNumbers = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                result.append(romanNumbers[i]);
+                number -= values[i];
+            }
+        }
+
+        return result.toString();
     }
 }

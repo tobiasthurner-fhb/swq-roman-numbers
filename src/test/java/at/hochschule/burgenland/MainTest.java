040cecf425e6e7ca3de12ceee172c77408e3a219
CUlreich-FHB <2410859028@hochschule-burgenland.at>
2026-05-02 09:51:08 +0200

    Initial commit

---
+++ src/test/java/at/hochschule/burgenland/MainTest.java
@@ -0,0 +1,12 @@
+package at.hochschule.burgenland;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}

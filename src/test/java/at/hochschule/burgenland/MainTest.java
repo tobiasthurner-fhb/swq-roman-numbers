07edc5eea792d3bd719e0bbc7a6d1f6328010016
Tobias Thurner <2410859009@hochschule-burgenland.at>
2026-05-02 11:40:16 +0200

    test: add parameterized tests

--- src/test/java/at/hochschule/burgenland/MainTest.java
+++ src/test/java/at/hochschule/burgenland/MainTest.java
@@ -1,43 +1,33 @@
 package at.hochschule.burgenland;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
-    @BeforeEach
-    void setUp() {
+    @ParameterizedTest
+    @CsvSource({
+            "1, I",
+            "2, II",
+            "4, IV",
+            "9, IX",
+            "11, XI",
+            "45, XLV",
+            "48, XLVIII",
+            "63, LXIII",
+            "399, CCCXCIX",
+            "944, CMXLIV",
+            "3000, MMM"
+    })
+    void toRomanNumbersConvertsDecimalNumbers(int number, String expectedRomanNumber) {
+        assertEquals(expectedRomanNumber, RomanNumberConverter.toRomanNumbers(number));
     }
 
-    @Test
-    void testSingleDigits(){
-        assertEquals("I", RomanNumberConverter.toRomanNumbers(1));
-    }
-
-    @Test
-    void testIdenticalDigits(){
-        assertEquals("II", RomanNumberConverter.toRomanNumbers(2));
-    }
-
-    @Test
-    void testBasicAddition(){
-        assertEquals("XI", RomanNumberConverter.toRomanNumbers(11));
-    }
-
-    @Test
-    void testBasicSubtraction(){
-        assertEquals("XLV", RomanNumberConverter.toRomanNumbers(45));
-    }
-
-    @Test
-    void testMultipleAddition(){
-        assertEquals("LXIII", RomanNumberConverter.toRomanNumbers(63));
-    }
-
-    @Test
-    void testMultipleSubstraction(){
-        assertEquals("XLVIII", RomanNumberConverter.toRomanNumbers(48));
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 3001})
+    void toRomanNumbersRejectsNumbersOutsideSupportedRange(int number) {
+        assertThrows(IllegalArgumentException.class, () -> RomanNumberConverter.toRomanNumbers(number));
     }
 }
